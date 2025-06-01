bbee079a1b42a9d88f6de5d357d58e086cab7f90
Álvaro Fernández Gallego <varogallego1998@gmail.com>
2025-06-01 23:45:20 +0200

    Update project

---
+++ project/src/Entity/Species.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpeciesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SpeciesRepository::class)]
+class Species
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $scientificName = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $commonName = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\ManyToOne(inversedBy: 'species')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categories $category = null;
+
+    #[ORM\ManyToOne(inversedBy: 'species')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Status $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScientificName(): ?string
+    {
+        return $this->scientificName;
+    }
+
+    public function setScientificName(string $scientificName): static
+    {
+        $this->scientificName = $scientificName;
+
+        return $this;
+    }
+
+    public function getCommonName(): ?string
+    {
+        return $this->commonName;
+    }
+
+    public function setCommonName(?string $commonName): static
+    {
+        $this->commonName = $commonName;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Categories
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Categories $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getStatus(): ?Status
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?Status $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
